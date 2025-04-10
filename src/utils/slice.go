3d811abfb0da1603fde6c962410575df2db6c610
Luc Drenth <luc.drenth@hotmail.com>
2025-04-10 20:47:11 +0200

    fix typo in comment

--- src/utils/slice.go
+++ src/utils/slice.go
@@ -1,6 +1,6 @@
 package utils
 
-// If there are no duplciates, returns (nil, -1, -1)
+// If there are no duplicates, returns (nil, -1, -1)
 // If there are duplicates, return the duplicate element and the array indices in which the duplicates are
 func GetFirstDuplicate[T comparable](typeIds []T) (*T, int, int) {
 	for i := range len(typeIds) {
