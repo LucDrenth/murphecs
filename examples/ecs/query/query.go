de20a8117d60a3db1a80b3b801cbdf84e2a6f83e
Luc Drenth <luc.drenth@hotmail.com>
2025-04-17 02:00:39 +0200

    add query example

---
+++ examples/ecs/query/query.go
@@ -0,0 +1,51 @@
+// Demonstrate how to query entities and components
+package main
+
+import (
+	"fmt"
+
+	"github.com/lucdrenth/murph_engine/src/ecs"
+)
+
+type NPC struct {
+	name string
+	ecs.Component
+}
+type Friendly struct{ ecs.Component }
+type Dialog struct {
+	text string
+	ecs.Component
+}
+
+func main() {
+	world := ecs.NewWorld()
+	ecs.Spawn(&world, NPC{name: "Murphy"}, Friendly{}, Dialog{text: "hi my name is Murphy"})
+	ecs.Spawn(&world, NPC{name: "Yuki"}, Dialog{text: "my name is Yuki"})
+	ecs.Spawn(&world, NPC{name: "Fiona"}, Friendly{}, Dialog{text: "hi my name is Fiona"})
+	ecs.Spawn(&world, NPC{name: "Bob"})
+
+	// Query all NPC components
+	ecs.Query[NPC](&world).
+		Iter(func(entityId ecs.EntityId, npc *NPC) error {
+			fmt.Printf("simple query: %d: %s \n", entityId, npc.name)
+			return nil
+		})
+
+	// Query all NPC components of entities that have both the Friendly and the Dialog component
+	ecs.Query[NPC](&world, ecs.With[Friendly](), ecs.With[Dialog]()).Iter(func(entityId ecs.EntityId, npc *NPC) error {
+		fmt.Printf("query with Friendly and Dialog: %d: %s \n", entityId, npc.name)
+		return nil
+	})
+
+	// Query all NPC and Dialog components of entities that do not have the Friendly component
+	ecs.Query2[NPC, Dialog](&world, ecs.Without[Friendly]()).Iter(func(entityId ecs.EntityId, npc *NPC, dialog *Dialog) error {
+		fmt.Printf("query without Friendly: %d: %s says %s \n", entityId, npc.name, dialog.text)
+		return nil
+	})
+
+	// Query all NPC and (optionally) Dialog component of all entities that do not have the friendly component
+	ecs.Query2[NPC, Dialog](&world, ecs.Optional[Dialog](), ecs.Without[Friendly]()).Iter(func(entityId ecs.EntityId, npc *NPC, dialog *Dialog) error {
+		fmt.Printf("query with optional Dialog: %d: %s has dialog %v \n", entityId, npc.name, dialog)
+		return nil
+	})
+}
