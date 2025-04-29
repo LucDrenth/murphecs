8c03c0ffe9e19e2631800b434304ba2950619a7d
Luc Drenth <luc.drenth@hotmail.com>
2025-04-29 13:15:46 +0200

    simplify ecs error declarations

--- src/ecs/errors.go
+++ src/ecs/errors.go
@@ -2,8 +2,10 @@ package ecs
 
 import "errors"
 
-var ErrEntityNotFound error = errors.New("entity not found")
-var ErrComponentNotFound error = errors.New("component not found")
-var ErrDuplicateComponent error = errors.New("duplicate component")
-var ErrComponentAlreadyPresent error = errors.New("component is already present")
-var ErrComponentIsNotAPointer error = errors.New("component is not a pointer")
+var (
+	ErrEntityNotFound          error = errors.New("entity not found")
+	ErrComponentNotFound       error = errors.New("component not found")
+	ErrDuplicateComponent      error = errors.New("duplicate component")
+	ErrComponentAlreadyPresent error = errors.New("component is already present")
+	ErrComponentIsNotAPointer  error = errors.New("component is not a pointer")
+)
