798021afd552e80b55b97f37295d09e3ae404148
Luc Drenth <luc.drenth@hotmail.com>
2025-05-18 17:18:02 +0200

    add test to assert that inserting component in storage works well with garbage collector

--- src/ecs/component_storage_test.go
+++ src/ecs/component_storage_test.go
@@ -1,6 +1,7 @@
 package ecs
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -48,9 +49,9 @@ func TestComponentRegistryInsert(t *testing.T) {
 		assert := assert.New(t)
 
 		world := DefaultWorld()
-
 		componentRegistry, err := createComponentStorage(4, ComponentIdFor[componentA](&world))
 		assert.NoError(err)
+
 		_, err = componentRegistry.insert(&componentA{})
 		assert.NoError(err)
 	})
@@ -59,13 +60,84 @@ func TestComponentRegistryInsert(t *testing.T) {
 		assert := assert.New(t)
 
 		world := DefaultWorld()
-
 		componentRegistry, err := createComponentStorage(4, ComponentIdFor[componentA](&world))
 		assert.NoError(err)
 
 		for range 10 {
 			_, err = componentRegistry.insert(&componentA{})
 			assert.NoError(err)
+		}
+	})
+
+	type componentWithPointers struct {
+		Component
+		name     *string
+		aMap     map[string]int
+		intSlice []*int
+	}
+
+	t.Run("works well with garbage collector", func(t *testing.T) {
+		assert := assert.New(t)
+
+		world := DefaultWorld()
+		componentRegistry, err := createComponentStorage(4, ComponentIdFor[componentWithPointers](&world))
+		assert.NoError(err)
+
+		name := "a name"
+		sliceLength := 100
+
+		{
+			item := componentWithPointers{
+				name: &name,
+				aMap: map[string]int{
+					"1": 1,
+					"3": 3,
+				},
+				intSlice: []*int{},
+			}
+			for i := range sliceLength {
+				item.intSlice = append(item.intSlice, &i)
+			}
+
+			componentRegistry.insert(&item)
+		}
+
+		{
+			// assert that component matches what we inserted
+			item, err := getComponentFromComponentStorage[componentWithPointers](&componentRegistry, 0)
+			assert.NoError(err)
+			assert.Equal(name, *item.name)
+			assert.Equal(
+				map[string]int{
+					"1": 1,
+					"3": 3,
+				},
+				item.aMap,
+			)
+			assert.Len(item.intSlice, 100)
+			for i := range sliceLength {
+				assert.Equal(i, *item.intSlice[i])
+			}
+		}
+
+		runtime.GC()
+
+		{
+			// assert that component matches what we inserted after garbage collection has run
+			item, err := getComponentFromComponentStorage[componentWithPointers](&componentRegistry, 0)
+			assert.NoError(err)
+			assert.Equal(name, *item.name)
+			assert.Equal(
+				map[string]int{
+					"1": 1,
+					"3": 3,
+				},
+				item.aMap,
+			)
+			assert.Len(item.intSlice, 100)
+			for i := range sliceLength {
+				assert.Equal(i, *item.intSlice[i])
+			}
 		}
 	})
 }
